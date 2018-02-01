eb0d921478a8d0273a0392535f1a7f5af1bb158a
kyutaelee <rbfjqn@gmail.com>
2018-02-01 17:21:59 +0900

    modified shop

--- onlineshop/urls.py
+++ onlineshop/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^cart/', include('cart.urls', namespace='cart')),
+    url(r'^coupons/', include('coupons.urls', namespace='coupons')),
+    url(r'^orders/', include('orders.urls', namespace='orders')),
+    url(r'^$', include('shop.urls', namespace='shop')),
+    #url(r'^accounts/', )
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
